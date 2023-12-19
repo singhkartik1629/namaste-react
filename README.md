e2ec0685a5d48fe8649fa4cc6777563e76387606
Kartik Pratap <singhkartik1629@gmail.com>
2023-12-20 00:45:23 +0530

    episode-03

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Namaste React
+JSX will always prevent cross site scripting attack i.e. sanitizes data which come as js
--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React Element -
 const heading = React.createElement("h1", {id: "heading"}, "Hello world from React!");
+
+// React Element using jsx - 
+const Title =() => (
+    <h1 id="heading">
+        Namaste React Using JSX
+    </h1>
+);
+
+// React Components-
+//  Class Based Component (OLD)
+//  Functional Component (NEW)
+
+
+// React Functional Component
+// when we compose one component into another component then it is known as component compositon
+const HeadingComponent = () => (
+    <div>
+        <Title/> {/* we can compose functional component as Title is composed */}
+        {heading} {/*way to compose React element into funcional compnent*/}
+        <h1>Namaste React Functional Component</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// React Element rendering is done like this
+// root.render(jsxHeading);
+
+// React Functional Component is rendered like
+root.render(<HeadingComponent/>);
