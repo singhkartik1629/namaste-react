a8e2a6a70b2a186ea7660c1cadfb2ac803562139
Kartik Pratap <singhkartik1629@gmail.com>
2023-12-21 02:20:56 +0530

    episode-04

--- index.js
+++ index.js
@@ -1,35 +1,329 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element -
-const heading = React.createElement("h1", {id: "heading"}, "Hello world from React!");
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo"
+                src="https://media.istockphoto.com/id/1038356020/vector/restaurant-icon.jpg?s=612x612&w=0&k=20&c=Tk_v3JuJA4lz_8ZRJi78xS4p75Idqt97uEtYJciVtFI="/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 
-// React Element using jsx - 
-const Title =() => (
-    <h1 id="heading">
-        Namaste React Using JSX
-    </h1>
-);
+const resList = [
+  {
+    info: {
+      id: "353956",
+      name: "Bakingo",
+      cloudinaryImageId: "0c53fd8f62e3d25ba56d1b4cfe572cef",
+      locality: "A Block",
+      areaName: "Sector 83",
+      costForTwo: "₹300 for two",
+      cuisines: ["Bakery", "Desserts", "Beverages", "Snacks"],
+      avgRating: 4.3,
+      parentId: "3818",
+      avgRatingString: "4.3",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 1.6,
+        serviceability: "SERVICEABLE",
+        slaString: "24 mins",
+        lastMileTravelString: "1.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-21 01:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+  },
+  {
+    info: {
+      id: "356221",
+      name: "KFC",
+      cloudinaryImageId: "f01666ac73626461d7455d9c24005cd4",
+      locality: "Hazipur",
+      areaName: "Hajipur",
+      costForTwo: "₹450 for two",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      avgRating: 4.2,
+      parentId: "547",
+      avgRatingString: "4.2",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 4.3,
+        serviceability: "SERVICEABLE",
+        slaString: "27 mins",
+        lastMileTravelString: "4.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-21 05:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "20% OFF",
+        subHeader: "UPTO ₹50",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+  },
+  {
+    info: {
+      id: "177495",
+      name: "La Pino'z Pizza",
+      cloudinaryImageId: "ugemttaiukyqfhxkfs9i",
+      locality: "Pathways School Road",
+      areaName: "Hajipur",
+      costForTwo: "₹300 for two",
+      cuisines: ["Pizzas", "Pastas", "Italian", "Desserts", "Beverages"],
+      avgRating: 3.9,
+      parentId: "4961",
+      avgRatingString: "3.9",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 30,
+        lastMileTravel: 4.6,
+        serviceability: "SERVICEABLE",
+        slaString: "30 mins",
+        lastMileTravelString: "4.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-20 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+  },
+  {
+    info: {
+      id: "377798",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Noida World One",
+      areaName: "Sector 83",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 3.9,
+      parentId: "721",
+      avgRatingString: "3.9",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 4.2,
+        serviceability: "SERVICEABLE",
+        slaString: "31 mins",
+        lastMileTravelString: "4.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-21 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "FREE ITEM",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    }
+  },
+  {
+    info: {
+      id: "254106",
+      name: "McDonald's",
+      cloudinaryImageId: "6dc3ed2ca21d71acff7c2a51dfe4c720",
+      locality: "Advant IT Park",
+      areaName: "Sector 135",
+      costForTwo: "₹400 for two",
+      cuisines: ["American"],
+      avgRating: 4.4,
+      parentId: "630",
+      avgRatingString: "4.4",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 5.7,
+        serviceability: "SERVICEABLE",
+        slaString: "27 mins",
+        lastMileTravelString: "5.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-20 23:45:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    }
+  },
+];
 
-// React Components-
-//  Class Based Component (OLD)
-//  Functional Component (NEW)
+const RestaurantCard = (props) => {
+    const {resData} = props;
 
+    const { name, cuisines, costForTwo, avgRating } = resData?.info;
+    const { deliveryTime } = resData?.info?.sla
+    return(
+        <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+            <img 
+            className="res-logo" 
+            alt="res-logo"
+            src={
+                "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+                +resData.info.cloudinaryImageId
+            }
+            />
+            <h3>{name}</h3>
+            <h4>{cuisines}</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{avgRating} Stars</h4>
+            <h4>{deliveryTime} minutes</h4>
+        </div>
+    )
+}
 
-// React Functional Component
-// when we compose one component into another component then it is known as component compositon
-const HeadingComponent = () => (
-    <div>
-        <Title/> {/* we can compose functional component as Title is composed */}
-        {heading} {/*way to compose React element into funcional compnent*/}
-        <h1>Namaste React Functional Component</h1>
-    </div>
-);
+const Body = () => {
+    return (
+      <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+          {resList.map((restaurant) => (
+            <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+          ))}
+        </div>
+      </div>
+    );
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// React Element rendering is done like this
-// root.render(jsxHeading);
-
-// React Functional Component is rendered like
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
