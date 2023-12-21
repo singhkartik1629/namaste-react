fbdb538bc3ec3121f85974a87dcc93b1aba1cd49
Kartik Pratap <singhkartik1629@gmail.com>
2023-12-22 00:21:50 +0530

    episode-05

---
+++ src/components/Body.js
@@ -0,0 +1,33 @@
+import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+import resList from "../utils/mockData";
+
+
+const Body = () => {
+    const [restaurantList, setRestaurantList ] = useState(resList);
+    
+    return (
+      <div className="body">
+        <div className="filter">
+          <button
+            className="filter-btn"
+            onClick={() => {
+              const filteredList = restaurantList.filter(
+                (res) => res.info.avgRating >= 4
+              );
+              setRestaurantList(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+        <div className="res-container">
+          {restaurantList.map((restaurant) => (
+            <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+          ))}
+        </div>
+      </div>
+    );
+}
+
+export default Body;
---
+++ src/utils/mockData.js
@@ -0,0 +1,245 @@
+const resList = [
+    {
+        "info": {
+          "id": "356221",
+          "name": "KFC",
+          "cloudinaryImageId": "f01666ac73626461d7455d9c24005cd4",
+          "locality": "Hazipur",
+          "areaName": "Hajipur",
+          "costForTwo": "₹450 for two",
+          "cuisines": [
+            "Burgers",
+            "Biryani",
+            "American",
+            "Snacks",
+            "Fast Food"
+          ],
+          "avgRating": 4.2,
+            "deliveryTime": 26,
+            }
+      },
+      {
+        "info": {
+          "id": "191407",
+          "name": "La Pino'z Pizza",
+          "cloudinaryImageId": "ftq8nmz1a2bzfzulmkis",
+          "locality": "ABC tower",
+          "areaName": "Sector 135",
+          "costForTwo": "₹300 for two",
+          "cuisines": [
+            "Pizzas",
+            "Pastas",
+            "Italian",
+            "Desserts",
+            "Beverages"
+          ],
+          "avgRating": 4,
+          "parentId": "4961",
+          "avgRatingString": "4.0",
+          "totalRatingsString": "1K+",
+            "deliveryTime": 29,
+        }
+      },
+      {
+        "info": {
+          "id": "377798",
+          "name": "Pizza Hut",
+          "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+          "locality": "Noida World One",
+          "areaName": "Sector 83",
+          "costForTwo": "₹350 for two",
+          "cuisines": [
+            "Pizzas"
+          ],
+          "avgRating": 3.9,
+          "parentId": "721",
+          "avgRatingString": "3.9",
+          "totalRatingsString": "1K+",
+            "deliveryTime": 30,
+        }
+      },
+      {
+        "info": {
+          "id": "254106",
+          "name": "McDonald's",
+          "cloudinaryImageId": "6dc3ed2ca21d71acff7c2a51dfe4c720",
+          "locality": "Advant IT Park",
+          "areaName": "Sector 135",
+          "costForTwo": "₹400 for two",
+          "cuisines": [
+            "American"
+          ],
+          "avgRating": 4.4,
+          "parentId": "630",
+          "avgRatingString": "4.4",
+          "totalRatingsString": "10K+",
+            "deliveryTime": 27,
+        }
+      },
+      {
+        "info": {
+          "id": "90872",
+          "name": "Burger King",
+          "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+          "locality": "North Eye",
+          "areaName": "Sector 74",
+          "costForTwo": "₹350 for two",
+          "cuisines": [
+            "Burgers",
+            "American"
+          ],
+          "avgRating": 4.3,
+          "parentId": "166",
+          "avgRatingString": "4.3",
+          "totalRatingsString": "10K+",
+            "deliveryTime": 27,
+            
+        }
+      },
+      {
+        "info": {
+          "id": "353956",
+          "name": "Bakingo",
+          "cloudinaryImageId": "0c53fd8f62e3d25ba56d1b4cfe572cef",
+          "locality": "A Block",
+          "areaName": "Sector 83",
+          "costForTwo": "₹300 for two",
+          "cuisines": [
+            "Bakery",
+            "Desserts",
+            "Beverages",
+            "Snacks"
+          ],
+          "avgRating": 4.3,
+          "parentId": "3818",
+          "avgRatingString": "4.3",
+          "totalRatingsString": "5K+",
+            "deliveryTime": 30,
+        }
+      },
+      {
+        "info": {
+          "id": "28797",
+          "name": "Domino's Pizza",
+          "cloudinaryImageId": "vhbeexcj4xkytbcghko9",
+          "locality": "A 3rd Block",
+          "areaName": "Sector 110",
+          "costForTwo": "₹400 for two",
+          "cuisines": [
+            "Pizzas",
+            "Italian",
+            "Pastas",
+            "Desserts"
+          ],
+          "avgRating": 4.2,
+          "parentId": "2456",
+          "avgRatingString": "4.2",
+          "totalRatingsString": "5K+",
+            "deliveryTime": 25,
+        }
+      },
+      {
+        "info": {
+          "id": "183883",
+          "name": "Khadak Singh Da Dhaba",
+          "cloudinaryImageId": "8144bf37947c161953f656308d1bfc30",
+          "locality": "Hazipur",
+          "areaName": "Hajipur",
+          "costForTwo": "₹300 for two",
+          "cuisines": [
+            "North Indian",
+            "Chinese",
+            "Mughlai",
+            "Snacks"
+          ],
+          "avgRating": 4.2,
+          "parentId": "13170",
+          "avgRatingString": "4.2",
+          "totalRatingsString": "10K+",
+            "deliveryTime": 30,
+        }
+      },
+      {
+        "info": {
+          "id": "45512",
+          "name": "Theobroma",
+          "cloudinaryImageId": "b033728dcb0101ceb19bff0e1e1f6474",
+          "locality": "Morna",
+          "areaName": "Sector 34",
+          "costForTwo": "₹400 for two",
+          "cuisines": [
+            "Bakery",
+            "Desserts"
+          ],
+          "avgRating": 4.5,
+          "parentId": "1040",
+          "avgRatingString": "4.5",
+          "totalRatingsString": "10K+",
+            "deliveryTime": 32,
+        }
+      },
+      {
+        "info": {
+          "id": "76663",
+          "name": "Subway",
+          "cloudinaryImageId": "1ace5fa65eff3e1223feb696c956b38b",
+          "locality": "Ach Market",
+          "areaName": "Sector 72",
+          "costForTwo": "₹350 for two",
+          "cuisines": [
+            "Salads",
+            "Snacks",
+            "Desserts",
+            "Beverages"
+          ],
+          "avgRating": 3.9,
+          "parentId": "2",
+          "avgRatingString": "3.9",
+          "totalRatingsString": "10K+",
+            "deliveryTime": 27,
+        }
+      },
+      {
+        "info": {
+          "id": "314442",
+          "name": "Natural Ice Cream",
+          "cloudinaryImageId": "ihfhw9umlilffphilf6d",
+          "locality": "B Block",
+          "areaName": "Sector 83",
+          "costForTwo": "₹150 for two",
+          "cuisines": [
+            "Ice Cream",
+            "Desserts"
+          ],
+          "avgRating": 4.6,
+          "veg": true,
+          "parentId": "2093",
+          "avgRatingString": "4.6",
+          "totalRatingsString": "500+",
+            "deliveryTime": 19,
+            
+        }
+      },
+      {
+        "info": {
+          "id": "400158",
+          "name": "NIC Ice Creams",
+          "cloudinaryImageId": "85825a6d74b1059a63a9b688de9f67ce",
+          "locality": "Ghari Shahdara",
+          "areaName": "Sector 135",
+          "costForTwo": "₹120 for two",
+          "cuisines": [
+            "Ice Cream",
+            "Desserts"
+          ],
+          "avgRating": 4.5,
+          "veg": true,
+          "parentId": "6249",
+          "avgRatingString": "4.5",
+          "totalRatingsString": "1K+",
+            "deliveryTime": 22,
+        }
+      },
+];
+
+export default resList;
