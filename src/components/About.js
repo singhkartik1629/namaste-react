dd028348cdc91209c44fda093238b1d4b07afafc
Kartik Pratap <singhkartik1629@gmail.com>
2024-01-17 01:34:06 +0530

    episode-08

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,27 @@
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is namaste react web series</h2>
-        </div>
-    );
-};
+import UserClass from "./UserClass"
+import { Component } from "react";
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+        console.log("Parent Constructor");
+    }
+
+    componentDidMount() {
+        console.log("Parent Component Mounted");
+    }
+
+    render() {
+        console.log("Parent Render");
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>This is namaste react web series</h2>
+    
+                <UserClass name="Kartik Pratap" location="Dhampur, Uttar Pradesh" />
+            </div>
+        );
+    }
+}
 
 export default About;
--- src/components/Body.js
+++ src/components/Body.js
@@ -21,10 +21,10 @@ const Body = () => {
     const json = await data.json();
 
     setRestaurantList(
-      json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
     );
     setfilteredRestaurant(
-      json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
     );
   };
 
